c4d5b5891cc911bc35b9497be3f2848802c4ad1c
Brian Brakefield <pngbrianb@gmail.com>
2019-01-16 23:13:39 -0800

    here.com data to renderable dataframe; commented and cleaned up notebooks

--- notebooks/python_scripts/render_closures.py
+++ notebooks/python_scripts/render_closures.py
@@ -1,5 +1,9 @@
 def render_closures(data):
+    # import modules for the python script:
+    import pandas as pd
     import gmaps
+    import time
+
     # layout options: draw a comfortable-sized map
     figure_layout = {
     'width': '700px',
@@ -10,6 +14,10 @@ def render_closures(data):
     # initialize map
     fig = gmaps.figure(layout=figure_layout)
 
+    # establish list for map markers
+    marker_coords = []
+    marker_labels = []
+
     # draw road closures
     for index,point in data.iterrows():
         # The parameters are mostly self-explanatory, but I will note that this uses
@@ -17,11 +25,24 @@ def render_closures(data):
         close = gmaps.directions_layer(start=data.iloc[index][0],end=data.iloc[index][1],
                                        travel_mode='WALKING',show_markers=False,
                                       stroke_color='Red', stroke_opacity=0.8)
+        # Google Maps API limits to 50 requests per second
+        time.sleep(0.4)
+        
+        # add this closure's layer
         fig.add_layer(close)
+        # check for label text:
+        if data.iloc[index][2] is not None:
+            # append label info (I'm not really happy with how these end up looking, but
+            # we're on a time crunch. Function over form, I say!)
+            marker_coords.append(data.iloc[index][0])
+            marker_labels.append(data.iloc[index][2])
 
-    # establish labeled markers
-    marker_layer = gmaps.marker_layer(locations=data['start'],
-                                  label=data['text'])
-    # and add them to the map
-    fig.add_layer(marker_layer)
+
+    # if marker_labels isn't empty
+    if marker_labels != []:
+        # establish labeled markers
+        marker_layer = gmaps.marker_layer(locations=marker_coords,
+                                  label=marker_labels)
+        # and add them to the map
+        fig.add_layer(marker_layer)
     return fig
